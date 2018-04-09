7bedd156dcbe28d57506bca93d8e705d2b89fdef
Sam De Legge <samd@Sams-MacBook-Pro.local>
2018-04-08 21:38:45 -0700

    update

---
+++ liri.js
@@ -0,0 +1,30 @@
+
+require("dotenv").config();
+
+var argOne = process.argv[2];
+var argTwo = process.argv[3];
+
+console.log(argOne);
+var keys = require('./keys.js');
+var twitter = require('twitter');
+var request = require('request');
+var fs = require("fs");
+
+var spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+ 
+
+
+
+ 
+//spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+  //  if ( err ) {
+    //    console.log('Error occurred: ' + err);
+      //  return;
+    //}
+ 
+    // Do something with 'data' 
+//});
+
