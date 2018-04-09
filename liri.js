550eaf475ec40927fb178aabf029cd705ea8464d
Sam De Legge <samd@Sams-MacBook-Pro.local>
2018-04-09 09:43:27 -0700

    No more errors

--- liri.js
+++ liri.js
@@ -6,11 +6,11 @@ var argTwo = process.argv[3];
 
 console.log(argOne);
 var keys = require('./keys.js');
-var twitter = require('twitter');
+var Twitter = require('twitter');
 var request = require('request');
 var fs = require("fs");
 
-var spotify = require("node-spotify-api");
+var Spotify = require("node-spotify-api");  
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
