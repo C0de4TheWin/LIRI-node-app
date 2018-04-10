390be351742ddda2870868c37ad89ac6d282791b
Sam De Legge <samd@Sams-MacBook-Pro.local>
2018-04-09 22:46:12 -0700

    push

--- liri.js
+++ liri.js
@@ -15,9 +15,6 @@ var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-//var movieName = process.argv[2];
-
-
 
 inquirer
     .prompt([
@@ -27,11 +24,8 @@ inquirer
             message: "Choose an option NOW:",
             choices: [new inquirer.Separator(), 'spotify-this-song', 'movie-this', 'my-tweets', 'do-what-it-says']
         }
-
     ])
     .then(function (response) {
-
-
 
         if (argOne === "spotify-this-song") {
             spotify.search({ type: 'track', query: argOne, }, function (err, data) {
@@ -48,21 +42,47 @@ inquirer
             var params = { screen_name: 'charlieEcheese' };
             client.get('statuses/user_timeline', params, function (error, tweets, response) {
                 if (error) {
-                   console.log("There has been an error");
+                    console.log("There has been an error");
                 }
-                    //console.log("\nHere are your latest Tweets", + tweets[0].user.screen_name);
-                   
-                   
-                    for (var i = 0; i < tweets.length; i++) {
-                        console.log("DATE: " + tweets[i].created_at + "\nCTV TWEET:" + tweets[i].text);
-                        console.log("----------------");
+                //console.log("\nHere are your latest Tweets", + tweets[0].user.screen_name);
 
-                    }
-                })};
+
+                for (var i = 0; i < tweets.length; i++) {
+                    console.log("DATE: " + tweets[i].created_at + "\nCTV TWEET:" + tweets[i].text);
+                    console.log("----------------");
+
+                }
+            })
+        };
 
         if (argOne === "movie this") {
+            var movieName = "";
 
+            for (var i = 2; i < nodeArgs.length; i++) {
 
+                if (i > 2 && i < nodeArgs.length) {
+
+                    movieName = movieName + "+" + nodeArgs[i];
+
+                }
+
+                else {
+                    movieName += nodeArgs[i];
+
+                }
+            }
+
+            var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+            console.log(queryUrl);
+
+            request(queryUrl, function (error, response, body) {
+
+                // If the request is successful
+                if (!error && response.statusCode === 200) {
+                    console.log("Release Year: " + JSON.parse(body).Year);
+                }
+            });
         };
 
         if (argOne === "do-what-it-says") {
@@ -73,6 +93,7 @@ inquirer
 
 
 
-    //console.log(data.tracks.items[0]);
+    //I still need to input the 'do what it says' portion. Unable to get proper information from Arrays/indexs
+    //to display. Would like to review this before class begins tomorrow.
 
 
